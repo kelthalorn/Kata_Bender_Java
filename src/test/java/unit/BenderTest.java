55f88fb57ae50aa4a5a21130d45abfc82cb8a664
kelthalorn <tibo.colin@gmail.com>
2018-07-02 17:31:29 +0200

    init Test

--- src/test/java/unit/BenderTest.java
+++ src/test/java/unit/BenderTest.java
@@ -22,29 +22,29 @@ public class BenderTest {
             bender.matrix[0][x] = 'X';
         }
 
-        for (int x = 0; x < width; x++) {
-            bender.matrix[height-1][x] = 'X';
-        }
-
         bender.matrix[1][0] = 'X';
-        bender.matrix[1][1] = '';
-        bender.matrix[1][2] = '';
-        bender.matrix[1][3] = '';
-        bender.matrix[1][4] = '';
+        bender.matrix[1][1] = '@';
+        bender.matrix[1][2] = 'E';
+        bender.matrix[1][3] = ' ';
+        bender.matrix[1][4] = '$';
         bender.matrix[1][width-1] = 'X';
 
         bender.matrix[2][0] = 'X';
-        bender.matrix[2][1] = '';
-        bender.matrix[2][2] = '';
-        bender.matrix[2][3] = '';
-        bender.matrix[2][4] = '';
+        bender.matrix[2][1] = ' ';
+        bender.matrix[2][2] = 'N';
+        bender.matrix[2][3] = ' ';
+        bender.matrix[2][4] = ' ';
         bender.matrix[2][width-1] = 'X';
 
         bender.matrix[3][0] = 'X';
-        bender.matrix[3][1] = '';
-        bender.matrix[3][2] = '';
-        bender.matrix[3][3] = '';
-        bender.matrix[3][4] = '';
+        bender.matrix[3][1] = 'X';
+        bender.matrix[3][2] = ' ';
+        bender.matrix[3][3] = ' ';
+        bender.matrix[3][4] = ' ';
         bender.matrix[3][width-1] = 'X';
+
+        for (int x = 0; x < width; x++) {
+            bender.matrix[height-1][x] = 'X';
+        }
 	}
 }
