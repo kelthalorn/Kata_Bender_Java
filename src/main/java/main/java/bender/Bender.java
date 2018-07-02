afd0015d426cb2517ef780715ec0cf5d351e865c
kelthalorn <tibo.colin@gmail.com>
2018-07-02 16:57:47 +0200

    init repo

---
+++ src/main/java/main/java/bender/Bender.java
@@ -0,0 +1,7 @@
+package main.java.bender;
+
+public class Bender {
+
+    public Bender() {
+    }
+}
---
+++ src/test/java/unit/BenderTest.java
@@ -0,0 +1,20 @@
+package unit;
+
+import main.java.bender.Bender;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BenderTest {
+
+    private Bender bender;
+
+    @Before
+    public void init() {
+        bender = new Bender();
+    }
+
+    @Test
+    public void () {
+    }
+}
