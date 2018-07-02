4793403507ec78191cb04f7121ec2b12457a4027
kelthalorn <tibo.colin@gmail.com>
2018-07-02 17:06:11 +0200

    update maven config

--- src/test/java/unit/BenderTest.java
+++ src/test/java/unit/BenderTest.java
@@ -1,20 +1,8 @@
 package unit;
 
 import main.java.bender.Bender;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
 public class BenderTest {
 
     private Bender bender;
-
-    @Before
-    public void init() {
-        bender = new Bender();
-    }
-
-    @Test
-    public void () {
-    }
 }
